f40abac1d8fffe2cd54ce421030eeca9fba8ceb4
Pascual Martinez-Gomez <pascual@nii.ac.jp>
2016-06-08 18:37:44 +0900

    Added an argument in prove.py to specify optionally the HTML graphical output, instead of printing it always to stderr

--- prove.py
+++ prove.py
@@ -18,6 +18,7 @@
 from __future__ import print_function
 
 import argparse
+import codecs
 import logging
 from lxml import etree
 import os
@@ -29,31 +30,24 @@ from visualization_tools import convert_doc_to_mathml
 
 def main(args = None):
     DESCRIPTION=textwrap.dedent("""\
-            categories_template.yaml should contain the semantic templates
-              in YAML format.
-            parsed_sentence.xml contains the parsed sentences. All CCG trees correspond
+            The input file sem should contain the parsed sentences. All CCG trees correspond
             to the premises, except the last one, which is the hypothesis.
-            If --arbi-types flag is specified, then the arbitrary specification of
-            coq_types is enabled. Thus, semantic rule assignments should contain a
-            a field such as:
-            - semantics: \P x.P(x)
-              category: NP
-              coq_type: Animal
-            If --auto-types is enabled, or no flag is specified, then the automatic
-            inference of types is enabled. This automatic inference relies on the naive
-            implementation in the sem/logic module of NLTK.
       """)
 
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=DESCRIPTION)
-    parser.add_argument("sem")
+    parser.add_argument("sem", help="XML input filename with semantics")
+    parser.add_argument("--graph_out", nargs='?', type=str, default="",
+        help="HTML graphical output filename.")
     parser.add_argument("--abduction", action="store_true", default=False)
     parser.add_argument("--gold_trees", action="store_true", default=False)
     args = parser.parse_args()
       
     if not os.path.exists(args.sem):
-        print('File does not exist: {0}'.format(args.expression_templates_filename))
+        print('File does not exist: {0}'.format(args.sem))
+        parser.print_help()
+        sys.exit(1)
     
     logging.basicConfig(level=logging.WARNING)
 
@@ -62,8 +56,11 @@ def main(args = None):
 
     inference_result, coq_scripts = prove_doc(doc)
     print(inference_result, file=sys.stdout)
-    html_str = convert_doc_to_mathml(doc, coq_scripts)
-    print(html_str, file=sys.stderr)
+
+    if args.graph_out:
+        html_str = convert_doc_to_mathml(doc, coq_scripts)
+        with codecs.open(args.graph_out, 'w', 'utf-8') as fout:
+            fout.write(html_str)
 
 if __name__ == '__main__':
     main()
