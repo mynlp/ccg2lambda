e03c2de1bf4ed44bf6b341bb760c7be4f98429ed
Pascual Martinez-Gomez <pascual@nii.ac.jp>
2017-05-30 16:29:30 +0900

    More interpretable error when NLTK logic parsing

--- scripts/logic_parser.py
+++ scripts/logic_parser.py
@@ -13,9 +13,16 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+import logging
 
 from nltk.sem.logic import LogicParser
+from nltk.sem.logic import LogicalExpressionException
 
 logic_parser = LogicParser(type_check=False)
 def lexpr(formula_str):
-    return logic_parser.parse(formula_str)
+    try:
+        expr = logic_parser.parse(formula_str)
+    except LogicalExpressionException as e:
+        logging.error('Failed to parse {0}. Error: {1}'.format(formula_str, e))
+        raise
+    return expr
